f4bd6dbcbc7333d64b246dbd321042a900adf256
iuhjui <1332852718@qq.com>
2019-03-19 15:17:14 +0800

    append root command support

--- cmd/coind/cmdtree.go
+++ cmd/coind/cmdtree.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"gitee.com/iuhjui/logger"
+	"github.com/spf13/cobra"
 )
 
 type CmdTree struct {
 	logger *logger.Logger
+	root   *cobra.Command
 }
 
 func NewCmdTree() *CmdTree {
 	tree := new(CmdTree)
 	return tree
+}
+
+func (tree *CmdTree) makeRoot() error {
+	root := new(cobra.Command)
+	root.Use = "coind"
+	root.Short = "Coind is a crypto currency keystone-coin of core node"
+	tree.root = root
+	return nil
 }
 
 func (tree *CmdTree) SetLogger(logger *logger.Logger) {
